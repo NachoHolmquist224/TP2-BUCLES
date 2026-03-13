5557aa847123d7af779107f237689dd439e09fad
Ignacio Holmquist <ignacioholmquist12@gmail.com>
2026-03-13 08:11:42 -0300

    Ejercicio2

---
+++ js/Ejercicio2.js
@@ -0,0 +1,32 @@
+let nota = parseInt(prompt("Ingresa tu calificación:"));
+
+switch (nota) {
+  case (nota = 0):
+  case (nota = 1):
+  case (nota = 2): 
+    alert("Muy deficiente");
+    break;
+
+  case (nota = 3):
+  case (nota = 4): 
+    alert("Insuficiente");
+    break;
+
+  case (nota = 5):
+  case (nota = 6):
+    alert("Suficiente");
+    break;
+
+  case (nota = 7):
+    alert("Bien");
+    break;
+
+  case (nota = 8):
+  case (nota = 9):
+    alert("Notable");
+    break;
+
+  case (nota = 10):
+    alert("Sobresaliente");
+    break;
+}
